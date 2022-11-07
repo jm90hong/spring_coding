3dc8a36a930a5b878e2cfa9301ebe5741f480658
jm90hong <jm90.hong@gmail.com>
2022-11-07 16:29:11 +0900

    1

---
+++ src/main/java/com/my/spring_coding/SpringCodingApplication.java
@@ -0,0 +1,13 @@
+package com.my.spring_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/spring_coding/SpringCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.spring_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
